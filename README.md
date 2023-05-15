b72419fd9cf14db39dc5a8ebf2619f7846db0871
Patrick <patick95@gmx.de>
2023-05-15 18:09:49 +0200

    wip

--- README.md
+++ README.md
@@ -9,20 +9,21 @@ Es ist nicht notwendig, dass sie exakt gleich aussieht.
 
 Zusätzlich hat deine Benutzeroberfläche folgende Funktionalitäten:
 1. Der Nutzer kann einen Vornamen und einen Nachnamen in die Textfelder "Vorname" 
-und "Nachname" eingeben. 
-Wenn der Nutzer auf eins der zwei Felder klickt, verschwindet der vorausgefüllte Text "Vorname", bzw. "Nachname".
+und "Nachname" eingeben.
 2. Wenn der Nutzer ein Geschlecht auswählt, wird die Auswahl dem Nutzer angezeigt.
 Es kann immer nur ein Geschlecht ausgewählt sein.
 3. Dem Nutzer wird das ausgewählte Alter angezeigt.
 4. Dem Nutzer wird das ausgewählte Gewicht angezeigt.
 5. Wenn der Nutzer auf den Button "Einreichen" drückt, wird überprüft, ob der Nutzer alle Eingaben (Vorname, Nachname, Geschlecht, Alter, Gewicht) 
-   ausgewählt hat. 
+   ausgefüllt hat. 
    2. Ist das der Fall, wird dem Nutzer eine Bestätigungsnachricht angezeigt, 
    die alle Eingaben noch einmal auflistet.
-   3. Ist das nicht der Fall, wird der Nutzer darauf hingewiesen, dass die Eingaben unvollständig sind.
+   3. Ist das <b>nicht</b> der Fall, wird der Nutzer darauf hingewiesen, dass die Eingaben unvollständig sind.
 6. Wenn der Nutzer auf den Button "Reset" drückt, werden alle Eingaben auf die Standardwerte zurückgestellt.
 
-Gestalte das Eingabeformular in Hinsicht auf Benutzerfreundlichkeit.
+Zusätzlich:  
+- Gestalte das Eingabeformular möglichst benutzerfreundlich.  
+- Wenn möglich, benutze String-Ressourcen.
 
 <p align="center">
     Abbildung 1: Eingabeformular
--- app/src/main/java/de/syntax_institut/form/MainActivity.kt
+++ app/src/main/java/de/syntax_institut/form/MainActivity.kt
@@ -1,12 +1,103 @@
 package de.syntax_institut.form
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.SeekBar
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    // name views
+    lateinit var firstName: EditText
+    lateinit var lastName: EditText
+
+    // gender views
+    lateinit var genderText: TextView
+    lateinit var genderFemale: Button
+    lateinit var genderMale: Button
+    lateinit var genderDiverse: Button
+
+    // age views
+    lateinit var ageText: TextView
+    lateinit var age: SeekBar
+
+    // size views
+    lateinit var sizeText: TextView
+    lateinit var size: SeekBar
+
+    // submit and reset views
+    lateinit var submit: Button
+    lateinit var submitText: TextView
+    lateinit var reset: Button
+
+    // vars
+    var selectedGender: String? = null
+    var selectedAge = 0.0
+    var selectedSize = 0.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Init
+        // name views
+        firstName = findViewById(R.id.etVorname)
+        lastName = findViewById(R.id.etNachname)
+
+        // gender views
+        genderText = findViewById(R.id.tvGeschlecht)
+        genderFemale = findViewById(R.id.bFrau)
+        genderMale = findViewById(R.id.bMann)
+        genderDiverse = findViewById(R.id.bDivers)
+
+        // age views
+        ageText = findViewById(R.id.tvAlter)
+        age = findViewById(R.id.sbAlter)
+
+        // size views
+        sizeText = findViewById(R.id.tvGewicht)
+        size = findViewById(R.id.sbGewicht)
+
+        // submit and reset views
+        submit = findViewById(R.id.bEinreichen)
+        submitText = findViewById(R.id.tvAntwort)
+        reset = findViewById(R.id.bReset)
+
+        // Set listener
+
+        // Button "Frau"
+        genderFemale.setOnClickListener {
+            selectedGender = "Frau"
+            genderText.text = getString(R.string.geschlecht, "Frau")
+        }
+
+        // Button "Mann"
+        genderMale.setOnClickListener {
+            selectedGender = "Mann"
+            genderText.text = getString(R.string.geschlecht, "Mann")
+        }
+
+        // Button "Divers"
+        genderDiverse.setOnClickListener {
+            selectedGender = "Divers"
+            genderText.text = getString(R.string.geschlecht, "Divers")
+        }
+
+        // Button "Einreichen"
+        submit.setOnClickListener {
+            submitText.text = getString(R.string.antwort, firstName.text, lastName.text, selectedGender, selectedAge, selectedSize)
+        }
+
+        // Seekbar "Alter"
+
+
+        // Seekbar "Gewicht"
+
+        // Button "Reset"
+
+
     }
 }
